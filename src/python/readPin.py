3918ab1dc9f4c1c38f23a20b1462e8423bc082c6
ibrown <ibrown@twitter.com>
2013-12-17 19:45:57 -0800

    sane version of python driver

--- src/python/readPin.py
+++ src/python/readPin.py
@@ -4,8 +4,33 @@ import sys
 
 GPIO.setmode(GPIO.BCM)
 
-pin=int(sys.argv[1])
+pinA=int(sys.argv[1])
+pinB=int(sys.argv[2])
 
-GPIO.setup(pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pinA,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pinB,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print GPIO.input(pin)
+pin_A_state=None
+pin_B_state=None
+
+while True:
+    tmp_A_state=GPIO.input(pinA)
+    tmp_B_state=GPIO.input(pinB)
+
+    if (tmp_A_state != pin_A_state):
+
+        pin_A_state=tmp_A_state
+
+
+        print "A: " + str(pin_A_state)
+        print "B: " + str(pin_B_state)
+
+
+    if (tmp_B_state != pin_B_state):
+
+        pin_B_state=tmp_B_state
+
+
+        print "A: " + str(pin_A_state)
+        print "B: " + str(pin_B_state)
+
